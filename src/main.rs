bc020164e886f5b6a405cb6e5b8f27fc40bcec74
Saath Satheeshkumar <saathsatheesh@gmail.com>
2024-02-24 19:04:03 +0000

    feat: Create socket connection function - Interact with desktop app process

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,61 @@
 use active_win_pos_rs::get_active_window;
 use csv::ReaderBuilder;
-// use csv::WriterBuilder;
+use csv::WriterBuilder;
+use serde_derive::{Deserialize, Serialize};
+use serde_with::serde_as;
+use serde_with::TimestampSeconds;
 use std::collections::HashMap;
 use std::error::Error;
 use std::io;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::io::prelude::*;
+use std::net::Shutdown;
+use std::os::unix::net::UnixListener;
+use std::os::unix::net::UnixStream;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
-use std::{thread, time};
+use std::thread;
+
+use std::time::{self, SystemTime};
+fn close_socket() -> Result<(), Box<dyn Error>> {
+    std::fs::remove_file("/tmp/screen-time-sock")?;
+    Ok(())
+}
+
+fn listen_for_connections(
+    listener: &UnixListener,
+    terminating_arc: &Arc<AtomicBool>,
+) -> Result<(), Box<dyn Error>> {
+    for stream in listener.incoming() {
+        if terminating_arc.load(Ordering::Relaxed) {
+            break;
+        }
+        match stream {
+            Ok(stream) => {
+                println!("new client!");
+                handle_client(stream)?;
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
+    Ok(())
+}
+fn create_socket() -> Result<UnixListener, Box<dyn Error>> {
+    let listener = UnixListener::bind("/tmp/screen-time-sock")?;
+    Ok(listener)
+}
+fn handle_client(mut stream: UnixStream) -> Result<(), Box<dyn Error>> {
+    let mut received = String::new();
+    stream.read_to_string(&mut received)?;
+    println!("{}", received);
+
+    let response = "Hello from the server!";
+    stream.write_all(response.as_bytes())?;
+    Ok(())
+}
 
 fn read_csv(csv_path: String) -> Result<(), Box<dyn Error>> {
     let mut rdr = ReaderBuilder::new().from_path(csv_path)?;
@@ -17,13 +66,28 @@ fn read_csv(csv_path: String) -> Result<(), Box<dyn Error>> {
     println!("Finish read_csv method");
     Ok(())
 }
-// fn write_data_to_csv(
-//     program_times: &HashMap<String, time::Duration>,
-//     csv_path: String,
-// ) -> Result<(), Box<dyn Error>> {
-//     let mut wtr_result = WriterBuilder::new().from_path(csv_path)?;
-//     Ok(())
-// }
+#[serde_as]
+#[derive(Deserialize, Serialize)]
+struct Row<'a> {
+    #[serde_as(as = "TimestampSeconds<i64>")]
+    timestamp: SystemTime,
+    application: &'a str,
+    duration: u64,
+}
+fn _write_data_to_csv(
+    _program_times: &HashMap<String, time::Duration>,
+    csv_path: String,
+) -> Result<(), Box<dyn Error>> {
+    let mut wtr = WriterBuilder::new().has_headers(true).from_path(csv_path)?;
+    for (program_name, duration) in _program_times {
+        wtr.serialize(Row {
+            timestamp: SystemTime::now(),
+            application: program_name,
+            duration: duration.as_secs(),
+        })?;
+    }
+    Ok(())
+}
 fn screen_time_daemon(program_times: &mut HashMap<String, time::Duration>) {
     match get_active_window() {
         Ok(active_window) => {
@@ -35,8 +99,6 @@ fn screen_time_daemon(program_times: &mut HashMap<String, time::Duration>) {
             } else {
                 program_times.insert(app_name.to_string(), time::Duration::from_secs(1));
             }
-            // println!("app name: {}", app_name);
-            // println!("active window: {:#?}", active_window);
         }
         Err(()) => {
             println!("error occurred while getting the active window");
@@ -45,18 +107,39 @@ fn screen_time_daemon(program_times: &mut HashMap<String, time::Duration>) {
 }
 
 fn main() -> Result<(), io::Error> {
+    let pid = std::process::id();
+    println!("PID of the current Rust program: {}", pid);
+    let program_finished = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&program_finished))?;
+    let child_program_finished = Arc::clone(&program_finished);
+
+    thread::Builder::new()
+        .name("screen_time_daemon".to_string())
+        .spawn(move || {
+            let listener = create_socket().unwrap();
+            listen_for_connections(&listener, &child_program_finished).unwrap();
+
+            println!("Closing socket...");
+            close_socket().unwrap();
+            println!("Socket closed!");
+        })?;
+
     let mut program_times: HashMap<String, time::Duration> = HashMap::new();
-    let term = Arc::new(AtomicBool::new(false));
-    // signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
-    signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&term))?;
     //
     // Keep executing as long the SIGTERM has not been called.
-    while !term.load(Ordering::Relaxed) {
+    while !program_finished.load(Ordering::Relaxed) {
         thread::sleep(time::Duration::from_secs(1));
         screen_time_daemon(&mut program_times);
     }
 
     println!("Sigint received!");
+    let mut stream = UnixStream::connect("/tmp/screen-time-sock")?;
+    stream.write_all(b"Terminating Stream")?;
+    println!("Terminating stream sent!");
+    stream
+        .shutdown(Shutdown::Both)
+        .expect("shutdown function failed");
+
     for (program_name, duration) in &program_times {
         println!("{}: {}", program_name, duration.as_secs());
     }
